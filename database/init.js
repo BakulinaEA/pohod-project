30afa366adaa27d7a3d190eb6d87cdf8ae5bd2d8
BakulinaEA <lizabakulina05@gmail.com>
2024-06-12 05:23:19 +0400

    startup database actions in container

--- database/init.js
+++ database/init.js
@@ -1,11 +1,3 @@
 // Create db for Authentication Users
 db = db.getSiblingDB('auth_db');
-
 db.createCollection('users')
-db.users.insertOne(
-    {
-        username: 'Hello',
-        password: 'World',
-        email: 'hello@world.com'
-    }
-)
