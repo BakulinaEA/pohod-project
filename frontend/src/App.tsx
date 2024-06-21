78d89bf4fe39b4a43b2cbc81a62806d3c58d3aad
BakulinaEA <lizabakulina05@gmail.com>
2024-06-21 18:37:56 +0400

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Registration from '@/pages/Registration'
 import VerifyEmail from './pages/VerifyEmail'
+import Dashboard from './pages/Dashboard'
 
 const App = () => (
   <Routes>
-    <Route path="/login" element={<Login />}></Route>
+    <Route path="/*" element={<Login />}></Route>
     <Route path="/register" element={<Registration />}></Route>
     <Route path="/email/verify/:code" element={<VerifyEmail />}></Route>
+    <Route path="/dashboard" element={<Dashboard />}></Route>
   </Routes>
 )
+
+// npm run dev 
 
 export default App
