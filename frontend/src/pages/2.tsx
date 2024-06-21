6ff95183fbd1d70b0d6a9967ededf2ec144f3b32
BakulinaEA <lizabakulina05@gmail.com>
2024-06-21 18:45:47 +0400

    add 2 page

---
+++ frontend/src/pages/2.tsx
@@ -0,0 +1,34 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    CardFooter
+  } from '@/components/ui/card'
+  
+  import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+  } from '@/components/ui/select'
+  
+  import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+  import { UserNav } from '@/components/user-nav'
+  import { Layout, LayoutBody } from '@/components/custom/layout'
+  
+  import { Input } from '@/components/ui/input'
+  import { Label } from '@/components/ui/label'
+  import { Button } from '@/components/ui/button'
+  
+  //https://github.com/satnaing/shadcn-admin/blob/main/src/components/user-nav.tsx
+  export default function Dashboard() {
+    return (
+      <Layout className="items-center h-full">
+        {/* твой 2 код */}
+      </Layout>
+    )
+  }
+  
